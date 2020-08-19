5e03271e2f5cb2d45a9ccf8bce701dc14e85a72b
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-19 22:36:26 +0530

    comment

---
+++ pics/src/component/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    render(){
+        return (
+        <div>
+            Search Bar
+            </div>
+        );
+          
+        
+    }
+}
+
+export default SearchBar;
