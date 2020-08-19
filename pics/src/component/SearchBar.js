59a8f84714b532afdf20f5782540c00cbe214252
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-19 22:55:18 +0530

    comment

--- pics/src/component/SearchBar.js
+++ pics/src/component/SearchBar.js
@@ -3,9 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component{
     render(){
         return (
-        <div>
-            Search Bar
-            </div>
+        <div className="ui segment">
+           <form className="ui form">
+               <div className="field">
+                     <label>Image Search</label>
+                   
+                     <input type="text"/>
+               </div>
+           </form>
+        </div>
         );
           
         
