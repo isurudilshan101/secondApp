448f9bf29691d25708f5654082031c9aa8f20cc2
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-25 08:55:38 +0530

    comment

--- pics/src/component/App.js
+++ pics/src/component/App.js
@@ -4,24 +4,24 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component{
 
-    onSearchSubmit(term){
-       
-        axios.get('https://api.unsplash.com/',{
+    state={images:[]};
+
+    async onSearchSubmit(term){
+       const response =await axios.get('https://api.unsplash.com/search/photos',{
         params:{query:term},
         headers:{
             Authorization: 'Client-ID D4OO0GGuie8h-MYimpIQs2dZERNcR3D2ezGBbrs-VhI'
         }
 
-        }).then(response=>{
-            console.log(response.data.results);
-
         });
+      this.setState({images:response.data.results});
     }
 
     render(){
     return (
     <div className="ui container" style={{marginTop:'10px'}}>
        <SearchBar onSubmit={this.onSearchSubmit}/>
+       Found:{this.state.images.length} images
     </div>
         
         );
